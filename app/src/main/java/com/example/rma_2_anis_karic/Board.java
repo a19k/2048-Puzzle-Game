1ec676e3a2ab3d7316f9fec3ede1a3a1deca69eb
Karić Anis <anekane0@gmail.com>
2025-05-09 02:34:01 +0200

    added tile.xml and started work on game board model

---
+++ app/src/main/java/com/example/rma_2_anis_karic/Board.java
@@ -0,0 +1,31 @@
+package com.example.rma_2_anis_karic;
+
+import java.util.Arrays;
+
+public class Board {
+
+    private int[][] grid;
+    private int dimension;
+
+    public Board(int dimension) {
+        this.grid = new int[dimension][dimension];
+        this.dimension = dimension;
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public void clear(){
+        for (int i = 0; i < dimension; i++)
+            for (int j = 0; j < dimension; j++)
+                this.grid[i][j] = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "grid=" + Arrays.toString(grid) +
+                '}';
+    }
+}
