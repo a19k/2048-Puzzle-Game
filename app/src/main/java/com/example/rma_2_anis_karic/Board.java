85f048f5e769724029f8873729c2455ea0fe4578
Karić Anis <anekane0@gmail.com>
2025-05-12 04:36:00 +0200

    connected UI to ViewModel, inflated grid tiles

--- app/src/main/java/com/example/rma_2_anis_karic/Board.java
+++ app/src/main/java/com/example/rma_2_anis_karic/Board.java
@@ -1,11 +1,14 @@
 package com.example.rma_2_anis_karic;
 
+import android.util.Log;
+
 import java.util.Arrays;
 
 public class Board {
 
-    private int[][] grid;
-    private int dimension;
+    private final int[][] grid;
+    private final int dimension;
+    private static final String TAG = "BOARD";
 
     public Board(int dimension) {
         this.grid = new int[dimension][dimension];
@@ -16,25 +19,51 @@ public class Board {
         return grid;
     }
 
+    public int getDimension() {
+        return dimension;
+    }
+
     public void clear(){
         for (int i = 0; i < dimension; i++)
             for (int j = 0; j < dimension; j++)
                 grid[i][j] = 0;
     }
 
-    private void addNewTiles(){
+    public void addNewTiles(){
+        //count empty tiles in grid
         int emptyTileCounter = 0;
-        for(int i = 0; i < dimension; i++)
+        for (int i = 0; i < dimension; i++)
             for (int j = 0; j < dimension; j++)
                 if (grid[i][j] == 0) emptyTileCounter++;
 
-        int tileOfChoice = (int) ((Math.random() * 100));
+        //choose random empty tile
+        int tileOfChoice =((int)(Math.random() * 100000)) % emptyTileCounter;
+
+        //choose value randomly 2->90%, 4->10%
+        int tileValue;
+        if(((int)(Math.random() * 10)) % 10 == 0) tileValue = 4;
+        else tileValue = 2;
+
+        //find the chosen tile and assign the new value
+        emptyTileCounter = 0;
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++)
+                if (grid[i][j] == 0) {
+                    if (emptyTileCounter == tileOfChoice) grid[i][j] = tileValue;
+                    emptyTileCounter++;
+                }
+        }
+
+        Log.d(TAG, this.toString());
     }
 
     @Override
     public String toString() {
-        return "Board{" +
-                "grid=" + Arrays.toString(grid) +
-                '}';
+        StringBuilder fullGrid = new StringBuilder();
+
+        for(int i = 0; i < dimension; i++)
+            fullGrid.append("Board{" + "grid=").append(Arrays.toString(grid[i])).append('}');
+
+        return fullGrid.toString();
     }
 }
--- app/src/main/java/com/example/rma_2_anis_karic/MainActivity.java
+++ app/src/main/java/com/example/rma_2_anis_karic/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.rma_2_anis_karic;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.gridlayout.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreDisplay;
+    private TextView hiScoreDisplay;
+    private Button undoButton;
+    private Button resetButton;
+    private Button settingsButton;
+    private GridLayout viewGrid;
+
+    private final TextView [][] gridTiles = new TextView[4][4];
+
+    private final Board board = new Board(4);
+    private LayoutInflater inflater;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inflater = LayoutInflater.from(this);
+
+        setUpViewReferences();
+        board.addNewTiles();
+        initBoard();
+
+
     }
+
+    private void setUpViewReferences(){
+        scoreDisplay = (TextView) findViewById(R.id.score);
+        hiScoreDisplay = (TextView) findViewById(R.id.hiscore);
+        undoButton = (Button) findViewById(R.id.undo);
+        resetButton = (Button) findViewById(R.id.reset);
+        settingsButton = (Button) findViewById(R.id.settings);
+        viewGrid = (GridLayout) findViewById(R.id.grid);
+    }
+
+    private void initBoard(){
+        int dimension = board.getDimension();
+        for (int row = 0; row < dimension; row++)
+            for (int col = 0; col < dimension; col++) {
+                View newTile = inflater.inflate(R.layout.tile,viewGrid,false);
+                TextView tileText = newTile.findViewById(R.id.tileText);
+
+                if (board.getGrid()[row][col] != 0) {
+                    tileText.setText(String.valueOf(board.getGrid()[row][col]));
+                    tileText.setBackgroundColor(Color.parseColor("#0288D1"));
+                }
+                else {
+                    tileText.setText("");
+                    tileText.setBackgroundColor(Color.WHITE);
+                }
+
+                GridLayout.LayoutParams parameters = new GridLayout.LayoutParams();
+                parameters.rowSpec = GridLayout.spec(row);
+                parameters.columnSpec = GridLayout.spec(col);
+
+                int dpValue = 5; // margin in dips
+                float d = this.getResources().getDisplayMetrics().density;
+                int margin = (int)(dpValue * d); // margin in pixels
+                parameters.setMargins(margin,margin,margin,margin);
+
+                newTile.setLayoutParams(parameters);
+
+                viewGrid.addView(newTile);
+                gridTiles[row][col] = tileText;
+            }
+    }
+
 }
